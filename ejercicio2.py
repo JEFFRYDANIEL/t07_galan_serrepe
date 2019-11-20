79965a5f755757dca3a435e58d418df9296639c7
JEFFRYDANIEL <jgalanasenmache@gmail.com>
2019-11-20 14:09:36 -0500

    se agrego ejercicio 2 de while

--- ejercicio2.py
+++ ejercicio2.py
@@ -8,7 +8,7 @@ estacion_invalida=True
 
 #si la estacion es invalida volver a pedir la estacion
 while(estacion_invalida):
-    estacion=input("Ingrese la estacion:(verano/invierno/otoño/primavera):")
+    estacion=input("Ingrese la estacion verano/invierno/otoño/primavera):"
     estacion_invalida=(estacion != "verano" and estacion != "invierno" and
                        estacion !="otoño" and estacion !="primavera")
 #fin while

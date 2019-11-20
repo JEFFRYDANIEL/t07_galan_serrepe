6773c3bf49e6c7139aee5ee5dd6117ded0431b8e
JEFFRYDANIEL <jgalanasenmache@gmail.com>
2019-11-20 14:04:08 -0500

    se agrego ejercicio 1 de while

---
+++ ejercicio2.py
@@ -0,0 +1,16 @@
+import os
+#declarar variables
+estacion=""
+
+#pedir la variable via argumento
+estacion=os.sys.argv[1]
+estacion_invalida=True
+
+#si la estacion es invalida volver a pedir la estacion
+while(estacion_invalida):
+    estacion=input("Ingrese la estacion:(verano/invierno/otoño/primavera):")
+    estacion_invalida=(estacion != "verano" and estacion != "invierno" and
+                       estacion !="otoño" and estacion !="primavera")
+#fin while
+print("fin del bucle")
+print("la estacion es:",estacion)

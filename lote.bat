ef410e99f11769d96b4040d271c8f08e9ebbc905
JEFFRYDANIEL <jgalanasenmache@gmail.com>
2019-11-24 15:46:28 -0500

    se agrego el lote.bat

---
+++ lote.bat
@@ -0,0 +1,61 @@
+python galan/ejercicio1.py 12.4 12.5 12.8
+python galan/ejercicio1.py 13.3 15.5 14.5
+python galan/ejercicio1.py 19.5 18.5 10.5
+python galan/ejercicio2.py verano
+python galan/ejercicio2.py otoño
+python galan/ejercicio2.py primavera
+python galan/ejercicio3.py rojo
+python galan/ejercicio3.py amarillo
+python galan/ejercicio3.py verde
+python galan/ejercicio4.py 13.5 12.6
+python galan/ejercicio4.py 19.8 20.7
+python galan/ejercicio4.py 26.6 34.8
+python galan/ejercicio5.py A
+python galan/ejercicio5.py B
+python galan/ejercicio5.py C
+python galan/ejercicio6.py ABCD
+python galan/ejercicio6.py ACBD
+python galan/ejercicio6.py ACDB
+python galan/ejercicio7.py DEZW
+python galan/ejercicio7.py DZEW
+python galan/ejercicio7.py DZWE
+python galan/ejercicio8.py MNOP
+python galan/ejercicio8.py MONP
+python galan/ejercicio8.py MOPN
+python galan/ejercicio9.py HIGJT
+python galan/ejercicio9.py HJIGT
+python galan/ejercicio9.py HIJGT
+python galan/ejercicio10.py KLOSX
+python galan/ejercicio10.py KOLSX
+python galan/ejercicio10.py KSLOX
+python galan/ejercicio11.py 10
+python galan/ejercicio11.py 24
+python galan/ejercicio11.py 28
+python galan/ejercicio12.py 35
+python galan/ejercicio12.py 39
+python galan/ejercicio12.py 54
+python galan/ejercicio13.py 20
+python galan/ejercicio13.py 30
+python galan/ejercicio13.py 41
+python galan/ejercicio14.py 34
+python galan/ejercicio14.py 56
+python galan/ejercicio14.py 66
+python galan/ejercicio15.py 21
+python galan/ejercicio15.py 43
+python galan/ejercicio15.py 65
+python galan/ejercicio16.py 13
+python galan/ejercicio16.py 14
+python galan/ejercicio16.py 18
+python galan/ejercicio17.py 45
+python galan/ejercicio17.py 48
+python galan/ejercicio17.py 51
+python galan/ejercicio18.py 23.4
+python galan/ejercicio18.py 12.9
+python galan/ejercicio18.py 43.3
+python galan/ejercicio19.py 20.8 12.8
+python galan/ejercicio19.py 32.2 67.6
+python galan/ejercicio19.py 12.3 12.4
+python galan/ejercicio20.py 11.1 23
+python galan/ejercicio20.py 15.5 54
+python galan/ejercicio20.py 16.6 34
+

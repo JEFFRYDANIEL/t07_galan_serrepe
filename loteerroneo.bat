558e8825b75729eccced96b689e807fbb8f61c1c
JEFFRYDANIEL <jgalanasenmache@gmail.com>
2019-11-24 15:52:12 -0500

    se agrego el lote.bat

---
+++ loteerroneo.bat
@@ -0,0 +1,20 @@
+python galan/ejercicio1.py 12 12 12
+python galan/ejercicio2.py cc
+python galan/ejercicio3.py rs
+python galan/ejercicio4.py 13 12
+python galan/ejercicio5.py jj
+python galan/ejercicio6.py cv
+python galan/ejercicio7.py xs
+python galan/ejercicio8.py aw
+python galan/ejercicio9.py cx
+python galan/ejercicio10.py av
+python galan/ejercicio11.py 10.1
+python galan/ejercicio12.py 35.5
+python galan/ejercicio13.py 20.2
+python galan/ejercicio14.py 34.5
+python galan/ejercicio15.py 21.4
+python galan/ejercicio16.py 13.5
+python galan/ejercicio17.py 45.3
+python galan/ejercicio18.py 23
+python galan/ejercicio19.py 20 12.8
+python galan/ejercicio20.py 11.1 23.7
